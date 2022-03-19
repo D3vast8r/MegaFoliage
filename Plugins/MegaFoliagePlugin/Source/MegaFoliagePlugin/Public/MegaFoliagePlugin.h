4e9319f37bd8350ae092227078a9ee6f4241d603
D3vast8r <51064058+D3vast8r@users.noreply.github.com>
2022-03-19 03:24:04 -0400

    Update

---
+++ Plugins/MegaFoliagePlugin/Source/MegaFoliagePlugin/Public/MegaFoliagePlugin.h
@@ -0,0 +1,25 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+class FMegaFoliagePluginModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+("Test This Is A Test") \
+
+
+
+
+
+{
+	("test")
+}
